ff4fe4a7e4208924ca2355722cd61e2fbdd9990b
Ameerali C <ameeralic95@gmail.com>
2023-07-10 13:48:41 +0530

    merge started

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -25,12 +25,17 @@
                                     </svg>
                                 </div>
                                 <input type="search" id="default-search"
+                                v-model="keywords"
                                     class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Enter your Keyword.." required>
                                 <!-- <button type="submit"
                             class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> -->
                             </div>
                         </div>
+                        <!-- <v-select 
+  :options=GeoTargetCodes 
+  label="Name" multiple 
+  v-model="selectedGeoTargetCodes"></v-select> -->
                         <div class="grid md:grid-cols-2 gap-2">
                             <div>
                                 <button id="dropdownSearchButton" data-dropdown-toggle="dropdownSearch"
@@ -57,23 +62,25 @@
                                                 </svg>
                                             </div>
                                             <input type="text" id="input-group-search"
+                                            v-model="GeoSearch"
+                                            @change="filterGeoCodes()"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 placeholder="Search regions">
                                         </div>
                                     </div>
                                     <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200"
                                         aria-labelledby="dropdownSearchButton">
-                                        <li>
+                                        <li v-for="item in GeoFilterCode" :key="item">
                                             <div
                                                 class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                                                 <input id="checkbox-item-11" type="checkbox" value=""
                                                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
                                                 <label for="checkbox-item-11"
-                                                    class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Bonnie
-                                                    Green</label>
+                                                    class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
+                                                    {{ item["Canonical Name"] }}</label>
                                             </div>
                                         </li>
-                                        <li>
+                                        <!-- <li>
                                             <div
                                                 class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
                                                 <input checked id="checkbox-item-12" type="checkbox" value=""
@@ -132,7 +139,7 @@
                                                     class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Roberta
                                                     Casas</label>
                                             </div>
-                                        </li>
+                                        </li> -->
                                     </ul>
                                     <!-- <a href="#"
                                     class="flex items-center p-3 text-sm font-medium text-red-600 border-t border-gray-200 rounded-b-lg bg-gray-50 dark:border-gray-600 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-red-500 hover:underline">
@@ -275,6 +282,8 @@
 import { RouterLink } from 'vue-router';
 import PublicPagesLayout from '../components/PublicPagesLayout/PublicPagesLayout.vue'
 
+import GeoTargetCodes from '../assets/geotarget-india.json'
+
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 
@@ -282,4 +291,35 @@ import { initFlowbite } from 'flowbite'
 onMounted(() => {
     initFlowbite();
 })
+</script>
+<script>
+import vSelect from "vue-select"
+export default {
+    data() {
+        return {
+            keywords:'',
+            GeoCodes: GeoTargetCodes,
+            GeoSearch:'',
+            GeoFilterCode:GeoTargetCodes
+        }
+    },
+    components:{
+    vSelect
+  },
+    computed:{
+        keywordsArray(){
+            return this.keyword.split(',')
+        },
+    },
+    methods: {
+        filterGeoCodes(){
+            console.log(this.GeoSearch);
+            this.GeoFilterCode = this.GeoSearch !== '' ? this.GeoCodes.filter(item => {
+                var text = item["Canonical Name"]
+                return text.startsWith(this.GeoSearch)
+                // console.log(text.startsWith(this.GeoSearch));
+            }) : this.GeoCodes
+        }
+    },
+}
 </script>
